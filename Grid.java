66bf9a81cb0fa8da7b11eb853a47f1da20072752
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 13:55:05 -0600

    repeats grid

--- Grid.java
+++ Grid.java
@@ -1,20 +1,24 @@
 public class Grid {
   public static void main(String[] args) {
-    for (int i = 0; i < 5; i++) {
-      System.out.print(i);
-      System.out.print(", ");
-    }
-    int height = 20;
-    int width = 50;
-    for (int j = 0; j < height; j++) {
-      for (int i = 0; i < width; i++) {
-        if ((i > 20 && i < 23) || j == 9) {
-          System.out.print(" ");
-        } else {
-          System.out.print("#");
+    System.out.println("Current dir: " + System.getProperty("user.dir"));
+    for (int k = 0; k < 100; k++) {
+      for (int j = 0; j < 15; j++) {
+        System.out.println("");
+        int x = 20;
+        int y = 9;
+        int height = 20;
+        int width = 50;
+        for (j = 0; j < height; j++) {
+          for (int i = 0; i < width; i++) {
+            if (i == x && j == y) {
+              System.out.print(" ");
+            } else {
+              System.out.print("#");
+            }
+          }
+          System.out.println("");
         }
       }
-      System.out.println("");
     }
   }
 }
