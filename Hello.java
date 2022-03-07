4ab7b65e141fd41968a131ba57b91e809941b158
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 13:08:58 -0600

    adds grid

--- Hello.java
+++ Hello.java
@@ -10,5 +10,11 @@ public class Hello {
       System.out.print(i);
       System.out.print(", ");
     }
+    for (int i = 0; i < 20; i++) {
+      for (int j = 0; j < 50; j++) {
+        System.out.print("#");
+      }
+      System.out.println("");
+    }
   }
 }
