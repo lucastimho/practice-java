aaf9658132c058fc7a174838defa0e0cb12063fa
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 13:29:13 -0600

    adds if and else statements

--- Hello.java
+++ Hello.java
@@ -14,9 +14,17 @@ public class Hello {
     int width = 50;
     for (int i = 0; i < height; i++) {
       for (int j = 0; j < width; j++) {
-        System.out.print("#");
+        if (j == 22) {
+          System.out.print("#");
+        } else {
+          System.out.print(" ");
+        }
       }
       System.out.println("");
+    }
+    int date = 13;
+    if (date == 13) {
+      System.out.print("It's an unlucky day");
     }
   }
 }
