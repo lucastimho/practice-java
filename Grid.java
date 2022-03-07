140726286377304820d574c0cfe767b50c915ac9
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 15:16:57 -0600

    reformats to better repeat grids and numbers the lines

--- Grid.java
+++ Grid.java
@@ -1,24 +1,25 @@
 public class Grid {
   public static void main(String[] args) {
-    System.out.println("Current dir: " + System.getProperty("user.dir"));
+    // System.out.println("Current dir: " + System.getProperty("user.dir"));
+    int x = 20;
+    int y = 9;
+    int height = 20;
+    int width = 50;
     for (int k = 0; k < 100; k++) {
-      for (int j = 0; j < 15; j++) {
+      for (int l = 0; l < 15; l++) {
         System.out.println("");
-        int x = 20;
-        int y = 9;
-        int height = 20;
-        int width = 50;
-        for (j = 0; j < height; j++) {
-          for (int i = 0; i < width; i++) {
-            if (i == x && j == y) {
-              System.out.print(" ");
-            } else {
-              System.out.print("#");
-            }
+      }
+      for (int j = 0; j < height; j++) {
+        for (int i = 0; i < width; i++) {
+          if (i == x && j == y) {
+            System.out.print(" ");
+          } else {
+            System.out.print("#");
           }
-          System.out.println("");
         }
+        System.out.println(j);
       }
     }
+    System.out.println("Current dir: " + System.getProperty("user.dir"));
   }
 }
