fbc08a2aa456557de4e028652461c42a22ca601c
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 12:56:48 -0600

    adds for loop

--- Hello.java
+++ Hello.java
@@ -6,5 +6,9 @@ public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello World");
     System.out.print("Hello World \n");
+    for (int i = 0; i < 5; i++) {
+      System.out.print(i);
+      System.out.print(", ");
+    }
   }
 }
