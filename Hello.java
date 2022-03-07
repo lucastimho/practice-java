5be12f1036cba238158cef40b92135f10c13aebd
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-07 12:51:07 -0600

    adds two methods to return to new line

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@
 
 public class Hello {
   public static void main(String[] args) {
-    System.out.print("Hello World");
+    System.out.println("Hello World");
+    System.out.print("Hello World \n");
   }
 }
