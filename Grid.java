53a7c2cc3098eff815540628066585b53ff9b3bd
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-08 14:07:52 -0600

    annotates and renames variables

--- Grid.java
+++ Grid.java
@@ -1,14 +1,16 @@
 public class Grid {
   public static void main(String[] args) {
-    // System.out.println("Current dir: " + System.getProperty("user.dir"));
-    int x = 20;
-    int y = 9;
+    System.out.println("Current dir: " + System.getProperty("user.dir"));
+    int x = 23;
+    int y = 7;
     int height = 20;
     int width = 50;
-    for (int k = 0; k < 100; k++) {
-      for (int l = 0; l < 15; l++) {
+    for (int frame = 0; frame < 100; frame++) {
+      // draws blank lines
+      for (int i = 0; i < 15; i++) {
         System.out.println("");
       }
+      // makes grid
       for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
           if (i == x && j == y) {
@@ -19,6 +21,7 @@ public class Grid {
         }
         System.out.println("");
       }
+      // puts computer to sleep for 0.5 seconds
       try {
         java.lang.Thread.sleep(500);
       } catch (Exception e) {
