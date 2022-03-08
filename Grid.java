fa83eb7a0e0bae0ed5019df4e6d155479ea46e30
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-08 15:28:07 -0600

    creates class for ball to organize code

--- Grid.java
+++ Grid.java
@@ -1,3 +1,30 @@
+class Ball {
+  int x;
+  int y;
+
+  int vx;
+  int vy;
+
+  void move(int width, int height) {
+    x += vx;
+    y += vy;
+    if (x >= width || x == 0) {
+      vx *= -1;
+    }
+    if (y >= height || y == 0) {
+      vy *= -1;
+    }
+  }
+
+  Ball() {
+    x = 23;
+    y = 7;
+
+    vx = 1;
+    vy = 1;
+  }
+}
+
 public class Grid {
   public static void take_a_nap(int duration) {
     // puts computer to sleep for 0.5 seconds
@@ -11,12 +38,9 @@ public class Grid {
 
   public static void main(String[] args) {
     System.out.println("Current dir: " + System.getProperty("user.dir"));
-    int x = 23;
-    int y = 7;
     int height = 20;
     int width = 50;
-    int vx = 1; // horizontal ball velocity
-    int vy = 1; // vertical ball velocity
+    Ball ball = new Ball();
     for (int frame = 0; frame < 100; frame++) {
       // draws blank lines
       for (int i = 0; i < 15; i++) {
@@ -25,7 +49,7 @@ public class Grid {
       // makes grid
       for (int j = 0; j < height; j++) {
         for (int i = 0; i < width; i++) {
-          if (i == x && j == y) {
+          if (i == ball.x && j == ball.y) {
             System.out.print(" ");
           } else {
             System.out.print("#");
@@ -35,14 +59,7 @@ public class Grid {
       }
       take_a_nap(100);
       // move hole diagonally
-      x += vx;
-      y += vy;
-      if (x >= width || x == 0) {
-        vx *= -1;
-      }
-      if (y >= height || y == 0) {
-        vy *= -1;
-      }
+      ball.move(width, height);
     }
   }
 }
