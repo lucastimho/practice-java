808ef6158036216a3fe35123a3ad085202de4a92
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-09 15:40:22 -0600

    adds stretch function

--- Shapes.java
+++ Shapes.java
@@ -13,8 +13,26 @@ class Point {
   }
 }
 
+class Rect {
+  Point tl;
+  Point br;
+
+  Rect() {
+    tl = new Point(0.0, 0.0);
+    br = new Point(1.0, 1.0);
+  }
+
+  void stretch(double horz, double vert) {
+    br.x += horz;
+    br.y += vert;
+  }
+}
+
 public class Shapes {
   public static void main(String[] args) {
     Point p = new Point(1.5, 2.0);
+    Rect r = new Rect();
+    Rect r2d2 = new Rect();
+    r2d2.stretch(3.0, 2.0);
   }
 }
