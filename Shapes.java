8de312a202d1acca58d7aa3edad97d0a1325e800
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-09 16:50:57 -0600

    reformats variables to match tutorial

--- Shapes.java
+++ Shapes.java
@@ -2,9 +2,9 @@ class Point {
   double x;
   double y;
 
-  Point(double xi, double yi) {
-    x = xi;
-    y = yi;
+  Point(double intial_x, double intial_y) {
+    x = intial_x;
+    y = intial_y;
   }
 
   void return_to_origin() {
@@ -14,24 +14,24 @@ class Point {
 }
 
 class Rect extends Shapes {
-  Point tl;
-  Point br;
+  Point topLeft;
+  Point bottomRight;
 
   Rect() {
     type = "rectangle";
-    tl = new Point(0.0, 0.0);
-    br = new Point(1.0, 1.0);
+    topLeft = new Point(0.0, 0.0);
+    bottomRight = new Point(1.0, 1.0);
   }
 
   void stretch(double horz, double vert) {
-    br.x += horz;
-    br.y += vert;
+    bottomRight.x += horz;
+    bottomRight.y += vert;
   }
 
   double area() {
     type = "area";
-    double w = br.x - tl.x;
-    double h = br.y - tl.y;
+    double w = bottomRight.x - topLeft.x;
+    double h = bottomRight.y - topLeft.y;
     return w * h;
   }
 }
@@ -54,16 +54,35 @@ public class Shapes {
   }
 
   public static void main(String[] args) {
-    Point p = new Point(1.5, 2.0);
-    Rect r = new Rect();
-    Rect r2d2 = new Rect();
-    r2d2.stretch(3.0, 2.0);
-    Triangle t = new Triangle();
-    System.out.print("The area of r is ");
-    System.out.println(r.area());
-    System.out.print("The area of r2d2 is ");
-    System.out.println(r2d2.area());
-    System.out.print("The type of shape t is ");
-    System.out.println(t.type);
+    // Point p = new Point(1.5, 2.0);
+    Rect rectina = new Rect();
+    double arina = rectina.area();
+    System.out.print("The area of arina is ");
+    System.out.println(arina);
+
+    Rect rectifer = new Rect();
+    rectifer.stretch(2.0, 3.0);
+    double arifer = rectifer.area();
+    System.out.print("The area of arifer is ");
+    System.out.println(arifer);
+
+    System.out.print("Rectina is a ");
+    System.out.println(rectina.type);
+    System.out.print("Rectifer is a ");
+    System.out.println(rectifer.type);
+    Triangle trina = new Triangle();
+    System.out.print("Trina is a ");
+    System.out.println(trina.type);
+    // Rect r = new Rect();
+    // Rect r2d2 = new Rect();
+    // r2d2.stretch(3.0, 2.0);
+    // Triangle t = new Triangle();
+    // System.out.print("The area of r is ");
+    // System.out.println(r.area());
+    // System.out.print("The area of r2d2 is ");
+    // System.out.println(r2d2.area());
+    // System.out.print("The type of shape t is ");
+    // System.out.println(t.type);
+
   }
 }
