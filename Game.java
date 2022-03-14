b5c5b58b27a7f61a5d654a4ef2b3a1d3462ad2eb
lucastimho <lucas.t.ho.lh@gmail.com>
2022-03-14 15:37:22 -0500

    adds tubes for game application

--- Game.java
+++ Game.java
@@ -23,15 +23,24 @@ class Model {
   int bird_y;
   double vert_vel;
   int time_since_flap;
+  int tube_x;
+  int tube_y;
 
   Model() {
-
+    tube_x = 200;
+    tube_y = 300;
   }
 
   void update() {
+    // Move the bird
     vert_vel += 1.5;
     bird_y += vert_vel;
     time_since_flap++;
+
+    // Move the tube
+    tube_x -= 8;
+    if (tube_x < -100)
+      tube_x = 500;
   }
 
   void onClick() {
@@ -44,6 +53,8 @@ class View extends JPanel {
   JButton b1;
   Image bird_image1;
   Image bird_image2;
+  Image tube_image1;
+  Image tube_image2;
   Model mod;
 
   View(Controller c, Model m) {
@@ -55,6 +66,8 @@ class View extends JPanel {
     try {
       bird_image1 = ImageIO.read(new File("bird1.png"));
       bird_image2 = ImageIO.read(new File("bird2.png"));
+      tube_image1 = ImageIO.read(new File("tube1.png"));
+      tube_image2 = ImageIO.read(new File("tube2.png"));
     } catch (Exception e) {
       e.printStackTrace(System.err);
       System.exit(1);
@@ -63,10 +76,14 @@ class View extends JPanel {
 
   public void paintComponent(Graphics g) {
     super.paintComponent(g);
+    // Draw the bird
     if (mod.time_since_flap < 5)
       g.drawImage(bird_image2, mod.bird_x, mod.bird_y, null);
     else
       g.drawImage(bird_image1, mod.bird_x, mod.bird_y, null);
+
+    // Draw the tube
+    g.drawImage(tube_image2, mod.tube_x, mod.tube_y, null);
   }
 }
 
